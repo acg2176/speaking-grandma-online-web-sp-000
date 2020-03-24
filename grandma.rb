0598e1eef35a75dd8e59878203db76b768b58ad4
Anna Guerrero <acg2176@columbia.edu>
2020-03-24 04:35:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string.upcase
+  if string.upcase == true
     return "NO, NOT SINCE 1938!"
   elsif string.upcase == "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN!"
